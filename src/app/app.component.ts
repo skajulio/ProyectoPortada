cf0d4e77dff1cd000d1be3c458c10797b9edd73f
Julio Isabel Martinez Leal <julio.leal@neoris.cxnetworks.net>
2018-07-26 12:25:21 -0500

    Funcion de RGB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  rgb1:number;
+  rgb2:number;
+  rgb3:number;
+  objectRGB:object;
+
+  construirRGB(){
+    this.rgb1 = Math.floor(Math.random()*255);
+    this.rgb2 = Math.floor(Math.random()*255);
+    this.rgb3 = Math.floor(Math.random()*255);
+    console.log(this.rgb1, this.rgb2, this.rgb3);
+
+    return `rgb(${this.rgb1}, ${this.rgb2}, ${this.rgb3})`;
+  }
+
+  enviarRGB(){
+    this.objectRGB = {
+      color1:this.rgb1,
+      color2:this.rgb2,
+      color3:this.rgb3      
+    }
+    return this.objectRGB;
+  }
 }
